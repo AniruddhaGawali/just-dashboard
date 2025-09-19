4f94731276c7bf9463e39fb70aa07adca74d405a
Aniruddha Gawali <aniruddhagawali03@gmail.com>
2025-09-19 11:35:00 +0530

    feat: implement DashboardSidebar component and update Home page layout

---
+++ just-dashboard/src/components/common/DashboardSidebar.tsx
@@ -0,0 +1,20 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+
+export function DashboardSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader></SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
